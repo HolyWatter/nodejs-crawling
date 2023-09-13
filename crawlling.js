33dfc85c1e741bfb85aa869a155319f5f70ce916
HolyWatter <qktjdtn97@icloud.com>
2023-09-13 21:45:09 +0900

    . ;

--- crawlling.js
+++ crawlling.js
@@ -2,21 +2,31 @@ import axios from "axios";
 import * as cheerio from "cheerio";
 
 const getHtml = async () => {
-  const res = await axios.get("");
+  const res = await axios.get(
+    "https://www.coupang.com/np/search?component=&q=물고기&channel=user",
+    {
+      headers: {
+        "Accept-Language": "ko-KR,ko;q=0.8,en-US;q=0.5,en;q=0.3",
+        "Accept-Encoding": "gzip, deflate, br",
+        Connection: "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        Pragma: "no-cache",
+        "Cache-Control": "no-cache",
+      },
+    }
+  );
   return res.data;
 };
 
 getHtml().then((res) => {
-  console.log(res.items);
-  // let ulList = [];
-  // const $ = cheerio.load(res);
-  // const bodyList = $("._autoComplete_basis_result_1cDj8 div ul li a");
-  // console.log(bodyList);
-  // bodyList.map((i, e) => {
-  //   ulList[i] = {
-  //     id: i,
-  //     keyword: $(e).text(),
-  //   };
-  // });
-  // console.log(ulList);
+  let ulList = [];
+  const $ = cheerio.load(res);
+  const bodyList = $(".search-related-keyword dd a");
+  bodyList.map((i, e) => {
+    ulList[i] = {
+      id: i,
+      keyword: $(e).text(),
+    };
+  });
+  console.log(ulList);
 });
